ffdae817e1d62dc0e92895ac7ecfcfdd678da3ac
Blackjackdeath <89104602+Blackjackdeath@users.noreply.github.com>
2021-12-20 19:23:47 +0200

    fix1

--- script/main.js
+++ script/main.js
@@ -6,7 +6,7 @@ let promise=model();
 promise.then(
     result=>{
         data=result;
-        render(data, 'Europe Asia Africa Americas Oceania', '');
+        getRegion(data, 'Europe Asia Africa Americas Oceania', '')
         document.getElementById('region').addEventListener('click', ()=>getRegion(data,document.forms.regions.elements.region.value,document.getElementById('search').value));
         document.getElementById('search').addEventListener('input', ()=>getRegion(data,document.forms.regions.elements.region.value,document.getElementById('search').value));
     },
