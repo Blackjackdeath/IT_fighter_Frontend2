da89b0ec3d57f366e3c0971e1b84e85096ec4ce2
Blackjackdeath <89104602+Blackjackdeath@users.noreply.github.com>
2021-12-20 19:20:41 +0200

    fix

--- script/main.js
+++ script/main.js
@@ -1,10 +1,12 @@
 let data;
 import { getRegion } from "./controller.js";
 import { model } from "./model.js";
+import  { render } from "./viem.js"
 let promise=model();
 promise.then(
     result=>{
         data=result;
+        render(data, 'Europe Asia Africa Americas Oceania', '');
         document.getElementById('region').addEventListener('click', ()=>getRegion(data,document.forms.regions.elements.region.value,document.getElementById('search').value));
         document.getElementById('search').addEventListener('input', ()=>getRegion(data,document.forms.regions.elements.region.value,document.getElementById('search').value));
     },
